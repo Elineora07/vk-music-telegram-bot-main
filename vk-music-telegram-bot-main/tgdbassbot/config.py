13fc04b6414ddda418df633d98d30e1d1a6c7d11
Elineora <109291643+Elineora07@users.noreply.github.com>
2022-07-17 22:03:06 +0500

    Add files via upload

---
+++ vk-music-telegram-bot-main/tgdbassbot/config.py
@@ -0,0 +1,4 @@
+API_TOKEN = "5294033051:AAFcuzlwA0T9Tv3PvOLVmWXN1Uk1WrFB5vI"
+
+commands = ['/start', '/song', '/artist', '/setlang',
+            '/settings', '/my', '/users', '/newpost']
---
+++ vk-music-telegram-bot-main/tgdbassbot/messages.py
@@ -0,0 +1,133 @@
+# START MESSAGES-------------------------------------------------------------------------
+
+
+start_messages = {
+    "RU": """
+Просто отправьте мне имя артиста и/или название композиции, и я найду эту композицию!
+/song — поиск по композициям
+/artist — поиск по исполнителям
+/setlang — изменить язык
+/settings — изменить настройки
+/my — Ваш плейлист
+                """,
+
+    "EN": """
+Just send me an artist and/or a song name and I will find music for you!
+/song — search by song title
+/artist — search for artist name
+/setlang — change language
+/settings — change settings
+/my — your playlist
+                """,
+    "UZ": """
+Menga qo'shiqchi yoki qo'shiq nomini yuboring va men sizga uni topib beraman !
+/song — qoʻshiq nomi boʻyicha qidirish
+/artist - rassom nomini qidiring
+/setlang - tilni o'zgartirish
+/settings — sozlamalarni oʻzgartirish
+/my — sizning pleylistingiz
+"""
+}
+
+# START MESSAGES END-------------------------------------------------------------------------
+
+
+# /SONG MESSAGES-----------------------------------------------------------------------------
+
+song_messages = {
+    "RU": "Какую композицию ищете?",
+    "EN": "What song are you looking for?",
+    "UZ": "Qaysi qo'shiqni izlayabsiz?"}
+# /SONG MESSAGES END-------------------------------------------------------------------------
+
+# /ARTIST MESSAGES-----------------------------------------------------------------------------
+artist_messages = {
+    "RU": "Какого исполнителя ищете?",
+    "EN": "What artist are you looking for?",
+    "ES": "qaysi qo'shiqchini qo'shiqlarini izlayabsiz?"}
+# /ARTIST MESSAGES END ------------------------------------------------------------------------
+
+
+# /SETLANG MESSAGES----------------------------------------------------------------------------
+setlang_messages = {
+    "RU": "Выберите новый язык",
+    "EN": "Choose a new language",
+    "Uz": "Tilni o'zgartirish"}
+# /SETLANG MESSAGES END------------------------------------------------------------------------
+
+# /SETTINGS MESSAGES----------------------------------------------------------------------------
+settings_messages = {
+    "RU": "Какие опции хотите изменить?",
+    "EN": "What options do you want to change?",
+    "UZ": "Qaysi sozlamalarni o'zgartirmoqchisiz?"}
+# /SETTINGS MESSAGES END------------------------------------------------------------------------
+
+# /NOTHING MESSAGE----------------------------------------------------------------------------
+nothing_messages = {
+    "RU": "Ничего не нашлось",
+    "EN": "Nothing was found",
+    "UZ": "Hechnima topilmadi"}
+# /SETTINGS MESSAGES END------------------------------------------------------------------------
+
+# IN FIRST PAGE MESSAGE-------------------------------------------------------------------------
+you_in_first_page_message = {
+    "RU": "Вы уже первой странице.",
+    "EN": "You are already the first page.",
+    "UZ": "Siz bosh sahifadasiz."}
+# IN FIRST PAGE MESSAGE END----------------------------------------------------------------------
+
+# NUMBER PAGE------------------------------------------------------------------------------------
+number_page_message = {
+    "RU": "<b>{0}-{1}</b>\n\n",
+    "EN": "<b>{0}-{1}</b>\n\n",
+    "UZ": "<b>{0}-{1}</b>\n\n"}
+# NUMBER PAGE END--------------------------------------------------------------------------------
+
+add_to_favourite = {
+    "RU": "Добавлено в Ваш плейлист",
+    "EN": "Add to your playlist",
+    "UZ": "O'zingiz uchun musiqa to'plami hosil qiling"}
+
+
+del_from_favourite = {
+    "RU": "Песня убрана из Вашего плейлиста",
+    "EN": "Song was delete from your playlist",
+    "UZ": "Musiqa to'plamingizdan musiqani o'chirish"}
+
+
+no_playlist = {
+    "RU": "Ваш плейлист еще пуст...",
+    "EN": "Your playlist is still empty...",
+    "UZ": "Musiqa to'plami bo'sh"}
+
+
+select_language = {
+    "RU": "Выбери язык",
+    "EN": "Choose a language",
+    "UZ": "Tilni tanlang"}
+
+
+change_language = {
+    "RU": "Выбери язык",
+    "EN": "Choose a language",
+    "UZ": "Tilni tanlang"
+}
+
+change_count_results = {
+    "RU": "Количество результатов: ",
+    "EN": "Number of results: ",
+    "UZ": "Natija raqami: "
+
+}
+
+del_button_with_hearts = {
+    "RU": "Убрать кнопки с сердечками: ",
+    "EN": "Remove buttons with hearts: ",
+    "UZ": "Yurakchani olib tashlang: "
+}
+
+settings_menu = {
+    "RU": "Меню настроек",
+    "EN": "Settings menu",
+    "UZ": "Menyu sozlamalairi"
+}
